f35ba00d223a869e4fbefbe1716d275ac3629a13
Nick <njaginicholas@gmail.com>
2024-12-01 16:15:31 +0000

    update 6 files

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 //connectDB
+const connectDB = require('./db/connect')
 
 // routers
 const authRouter = require('./routes/auth')
@@ -26,6 +27,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
