061fa1faa70576c4df90c39287027f2bc695856c
Nick <njaginicholas@gmail.com>
2024-12-02 21:11:35 +0300

    update User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
+const { genSalt } = require('bcryptjs')
 const mongoose = require('mongoose')
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -22,5 +25,21 @@ const UserSchema = new mongoose.Schema({
         minlength:6
     },
 })
+
+UserSchema.pre('save', async function (){
+    const salt = await genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({userID: this._id, name: this.name}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME
+    })
+}
+
+UserSchema.methods.comparePassword = async function(candidatePassword){
+    const isMatch = await bcrypt.compare(candidatePassword,this.password)
+    return isMatch
+}
 
 module.exports = mongoose.model('user', UserSchema)
