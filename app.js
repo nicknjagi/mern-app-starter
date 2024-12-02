1dcd49183f19847412f13de680bcad69317e39ba
Nick <njaginicholas@gmail.com>
2024-12-02 21:11:01 +0300

    update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 //connectDB
 const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
 
 // routers
 const authRouter = require('./routes/auth')
@@ -18,7 +19,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobsRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
