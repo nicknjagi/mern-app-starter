311b2727ae51334b84e40056e8eae52bfd598bf0
Nick <njaginicholas@gmail.com>
2024-12-09 22:24:12 +0300

    update Job.js

--- models/Job.js
+++ models/Job.js
@@ -5,5 +5,22 @@ const JobSchema = new mongoose.Schema({
     type: String,
     required:[true, 'Please provide company name'],
     maxlength:50
+  },
+  position: {
+    type: String,
+    required:[true, 'Please provide position'],
+    maxlength:100
+  },
+  status: {
+    type:String,
+    enum:["interview","declined",'pending'],
+    default:"pending"
+  },
+  createdBy: {
+    type:mongoose.Types.ObjectId,
+    ref:'User',
+    required:[true, "Please provide user"]
   }
-})
+}, {timestamps:true})
+
+module.exports = mongoose.model('job', JobSchema)
