5150aa064d9cee0c94b2a8d1deb6a23ae4c3c627
Nick <njaginicholas@gmail.com>
2024-12-09 22:23:42 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// xtra security packaged
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 //connectDB
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
 
-// routers
+// routers,
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
@@ -16,7 +23,17 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 // extra packages
-
+app.set('trust proxy', 1 /* number of proxies between user and server */)
+app.use(rateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
@@ -24,7 +41,7 @@ app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
